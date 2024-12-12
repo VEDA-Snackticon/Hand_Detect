6a8aa2e411fb2a1a0fa9f16a4ea49a85a420fa70
Hyeonguk <khuk0114@naver.com>
2024-12-12 20:01:44 +0900

    Update yolov8main.cpp

--- src/yolov8main.cpp
+++ src/yolov8main.cpp
@@ -13,35 +13,6 @@
 
 #include <unordered_map>
 
-//임시
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-// 키 입력 상태를 비차단으로 설정하는 함수
-void enableNonBlockingInput() {
-    struct termios t;
-    tcgetattr(STDIN_FILENO, &t);       // 현재 터미널 설정 읽기
-    t.c_lflag &= ~(ICANON | ECHO);    // 비캐논 모드와 에코 제거
-    tcsetattr(STDIN_FILENO, TCSANOW, &t); // 변경 적용
-
-    // 파일 디스크립터를 비차단 모드로 설정
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
-}
-
-// 키 입력 상태를 원래대로 복원하는 함수
-void disableNonBlockingInput() {
-    struct termios t;
-    tcgetattr(STDIN_FILENO, &t);
-    t.c_lflag |= (ICANON | ECHO);     // 캐논 모드와 에코 복원
-    tcsetattr(STDIN_FILENO, TCSANOW, &t);
-
-    // 파일 디스크립터를 차단 모드로 복원
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, flags & ~O_NONBLOCK);
-}
-
 static int target_size = 640;
 static int framerate = 30;
 static YoloV8* yolov8 = new YoloV8(target_size);
@@ -63,7 +34,7 @@ static int CNT = 0;
 bool check_hand_exit(const std::set<int>& previous_ids, const std::set<int>& current_ids, std::set<int>& completed_tracks)
 {
     for (int id : previous_ids) {
-        if (current_ids.find(id) == current_ids.end()){// && completed_tracks.find(id) == completed_tracks.end()) {
+        if (current_ids.find(id) == current_ids.end() && completed_tracks.find(id) == completed_tracks.end()) {
             // completed_tracks.insert(id);
             std::cout<< "Hand exited: ID " << id << std::endl; //디버그 메시지
             return true; // 손이 들어왔다 나간 것을 감지
@@ -76,32 +47,14 @@ extern "C"
 {
     int event_main(uint8_t* buffer, size_t size, time_t timestamp, int width, int height)
     {
-        enableNonBlockingInput();
+        std::this_thread::sleep_for(std::chrono::seconds(60));
+        std::cout<<"hand detected"<<std::endl;
+        return 1;
 
-        char key;
-        // bool running = true;
+    }
 
-        // while (running) {
-            // 키 입력 확인
-            key = getchar();
-            if (key == 'q') { // 'q' 키가 입력되면
-                // running = false;
-                std::cout << "Hand entered and exited the frame." << std::endl;
-                disableNonBlockingInput(); // 비차단 입력 비활성화
-                return 1;                  // 프로그램 종료
-            }
-
-            // 다른 동작 수행 (예: 출력 작업)
-            // std::cout << "Program is running. Press 'q' to quit.\r";
-            // std::cout.flush();
-
-            // usleep(100000); // 0.1초 대기
-        // }
-
-        // 비차단 입력 비활성화
-        disableNonBlockingInput();
+}
         
-        /*
         // CNT++;
         if(buffer == nullptr){
             return -1;
@@ -156,11 +109,11 @@ extern "C"
         }
 
 
-        if((CNT % 100) == 0){
+        // if((CNT % 100) == 0){
             
-            std::cout<<"Event ocurred"<<std::endl;
-            return 1;
-        }
+        //     std::cout<<"Event ocurred"<<std::endl;
+        //     return 1;
+        // }
 
 
         // debug
@@ -191,6 +144,5 @@ extern "C"
         previous_ids = current_ids;
 
         return rtn;
-        */
     }
 }
